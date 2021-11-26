c367a24b886b7fa77ccf80eb8fc5bba2fc0ad50e
Andrew Henry <andrewmhenry@me.com>
2021-11-26 15:27:24 +0800

    :heavy_plus_sign: feat(Connect to Solana Contract): added IDL and solana web3 and project-serum

--- react/src/App.js
+++ react/src/App.js
@@ -1,3 +1,6 @@
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js'
+import { Program, Provider, web3 } from '@project-serum/anchor'
+import idl from './idl.json'
 import { useEffect, useState } from 'react'
 import twitterLogo from './assets/twitter-logo.svg'
 import './App.css'
